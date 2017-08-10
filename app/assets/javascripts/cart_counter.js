b13119d58be9d39b1e55c32601b23748cc70e57a
domi91c <dominic.n@me.com>
2017-08-10 10:27:32 -0400

    make cart counter run on page load

--- app/assets/javascripts/cart_counter.js
+++ app/assets/javascripts/cart_counter.js
@@ -1,4 +1,4 @@
-$(function() {
+document.addEventListener('turbolinks:load', function() {
   $('#product-count-decrease').click(function() {
     console.log('decrease count test');
     $('#product-count').val(Math.max(1, Number($('#product-count').val()) - 1));
