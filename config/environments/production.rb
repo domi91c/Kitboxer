1af8e689b4ea46276f423ef9086810be8bca9ecb
domi91c <dominic.n@me.com>
2017-08-14 18:53:10 -0400

    fixing sendgrid on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'kitboxer.heroku.com' }
+
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
