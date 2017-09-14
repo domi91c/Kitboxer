ffb5ae8c8ee43b17978de62cb4dbe7cd61730234
domi91c <dominic.n@me.com>
2017-09-13 20:15:37 -0400

    attempt another heroku push

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -3,13 +3,13 @@
 
 document.addEventListener('turbolinks:load', function() {
   $('.js-quantity-decrease').click(function() {
-    let id = $(this).closest('[data-product-id]').data('product-id')
-    let quantityField = $('tr').find(`[data-product-id="${id}"]`)
+    var id = $(this).closest('[data-product-id]').data('product-id')
+    var quantityField = $('tr').find(`[data-product-id="${id}"]`)
     quantityField.val(Math.max(1, Number(quantityField.val()) - 1))
   })
   $('.js-quantity-increase').click(function() {
-    let id = $(this).closest('[data-product-id]').data('product-id')
-    let quantityField = $('tr').find(`[data-product-id="${id}"]`)
+    var id = $(this).closest('[data-product-id]').data('product-id')
+    var quantityField = $('tr').find(`[data-product-id="${id}"]`)
     quantityField.val(Math.max(1, Number(quantityField.val()) + 1))
   })
 })
