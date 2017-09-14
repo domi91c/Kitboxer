5bbde78cd1d63096ef26cc58c90429a2a1cb36fe
domi91c <dominic.n@me.com>
2017-09-14 13:57:07 -0400

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,6 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
