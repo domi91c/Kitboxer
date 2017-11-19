547775f9adb3acbde3d3cc01c65d60a65a7a9711
domi91c <dominic.n@me.com>
2017-11-18 21:55:56 -0500

    random adjustments

--- app/javascript/avatar/model.js
+++ app/javascript/avatar/model.js
@@ -1,31 +1,33 @@
-import * as axios from 'axios';
+import * as axios from 'axios'
 
 axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector(
-    'meta[name="csrf-token"]').getAttribute('content');
+    'meta[name="csrf-token"]').getAttribute('content')
 
-var BASE_URL = '';
+var BASE_URL = ''
 if (window.environment === 'development') {
-  BASE_URL = 'http://localhost:3001';
+  BASE_URL = 'http://localhost:3001'
 } else if (window.environment === 'production') {
-  BASE_URL = 'http://138.197.153.164';
+  BASE_URL = 'http://138.197.153.164'
 }
 
 function loadAvatar(memberId) {
-  const url = `${BASE_URL}/avatars`;
-  return axios.get(url, { params: { user_id: memberId } }).then((x) => {
-    return x;
-  }).catch(function(error) {
-    console.log(error);
-  });
+  const url = `${BASE_URL}/avatars`
+  return axios.get(url, { params: { user_id: memberId } })
+              .then((x) => {
+                return x
+              })
+              .catch(function(error) {
+                console.log(error)
+              })
 }
 
 function uploadAvatar(formData) {
-  const url = `${BASE_URL}/avatars`;
+  const url = `${BASE_URL}/avatars`
   return axios.post(url, formData).then(x => {
-    return JSON.parse(x.request.response);
+    return JSON.parse(x.request.response)
   }).catch((error) => {
-    console.log(error);
-  });
+    console.log(error)
+  })
 }
 
-export { loadAvatar, uploadAvatar } ;
+export { loadAvatar, uploadAvatar }
