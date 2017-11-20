74071a3257c09386170fe72e5e1bef41be2c6690
domi91c <dominic.n@me.com>
2017-11-20 10:41:12 -0500

    worked on profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :orders
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
   resources :users
   resources :charges
   resources :purchases
