2c8f6cbe2e17385b9ebc4b3f67d44391c0cf9649
domi91c <dominic.n@me.com>
2017-12-10 04:36:38 -0500

    fixed es6 error preventing heroku push

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -3,28 +3,12 @@
 
 document.addEventListener('turbolinks:load', function() {
   $('.js-quantity-decrease').click(function() {
-    let quantityField = $(this).siblings('.js-quantity-input')
+    var quantityField = $(this).siblings('.js-quantity-input')
     quantityField.val(Math.max(0, Number(quantityField.val()) - 1))
   })
   $('.js-quantity-increase').click(function() {
-    console.log(this)
-    let quantityField = $(this).siblings('.js-quantity-input')
+    var quantityField = $(this).siblings('.js-quantity-input')
     quantityField.val(Math.max(0, Number(quantityField.val()) + 1))
   })
 })
-
-// document.addEventListener('turbolinks:load', function() {
-//   $('.js-quantity-decrease').click(function() {
-//     console.log(this)
-//     var id = $(this).closest('[data-product-id]').data('product-id')
-//     var quantityField = $('form').find('[data-product-id="' + id + '"]')
-//
-//     quantityField.val(Math.max(1, Number(quantityField.val()) - 1))
-//   })
-//   $('.js-quantity-increase').click(function() {
-//     var id = $(this).closest('[data-product-id]').data('product-id')
-//     var quantityField = $('form').find('[data-product-id="' + id + '"]')
-//     quantityField.val(Math.max(1, Number(quantityField.val()) + 1))
-//   })
-// })
 
