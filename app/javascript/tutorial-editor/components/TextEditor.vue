17c354c644e6373699a786ee035ae1e308f2307f
domi91c <dominic.n@me.com>
2018-04-13 23:13:24 -0400

    dropped spree tables

---
+++ app/javascript/tutorial-editor/components/TextEditor.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input :id="`trix-input-${_uid}`" type="hidden" :value="value">
+        <trix-editor class="form-control" ref="`trix${_uid}`" :placeholder="placeholder" :input="`trix-input-${_uid}`"
+                     @trix-change="change"></trix-editor>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    props: ['value', 'placeholder'],
+    methods: {
+      change({ target }) {
+        this.$emit('input', target.value)
+      },
+    },
+  }
+</script>
---
+++ app/jobs/sample_job.rb
@@ -0,0 +1,4 @@
+class SampleJob
+  def self.run
+  end
+end
