f40c6f12ef125e212c1f739116f54441a20a07f4
domi91c <dominic.n@me.com>
2018-04-14 21:29:15 -0400

    forgot to add bucket name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
-  config.aws_acl    = 'public-read'
+  config.storage = :aws
+  config.aws_bucket = ENV['S3_BUCKET_NAME']
+  config.aws_acl = 'public-read'
 
   # The maximum period for authenticated_urls is only 7 days.
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
@@ -13,10 +13,10 @@ CarrierWave.configure do |config|
   }
 
   config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION'), # Required
-      stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'], # Required
+      stub_responses: Rails.env.test? # Optional, avoid hitting S3 actual during tests
   }
 
   # Optional: Signing of download urls, e.g. for serving private content through
