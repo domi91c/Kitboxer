861dee5e9e7380f5f5ccd3a85819f9f7cfe42ace
domi91c <dominic.n@me.com>
2018-07-15 02:02:18 -0400

    removed bootsnap

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -25,6 +25,7 @@ Rails.application.config.content_security_policy do |policy|
   if Rails.env.development? || Rails.env.test?
     policy.script_src :self, :https, :unsafe_eval
   else
-    policy.script_src :self, :httpsend
+    policy.script_src :self, :https
   end
+
 end
